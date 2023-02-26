2dfd6e9deff5866b09b26b1d038ebe7f1bc17567
thapaRoyal <thaparoyal17@gmail.com>
2023-02-26 10:25:31 -0500

    cards components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat tree',
+      imageUrl:
+        'https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'snowy moutain',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      username: 'mountain',
+      content: 'This is a great mountain',
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl:
+        'https://images.unsplash.com/photo-1508789454646-bef72439f197?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlraW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      username: 'biket',
+      content: 'Biking is great',
+    },
+  ];
 }
